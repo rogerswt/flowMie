1a50f345959135c5398b5589e7e3795d93da463a
Wade Rogers <rogersw@upenn.edu>
2020-02-09 12:04:47 -0500

    hunh?

---
+++ vignettes/flowMie-package.R
@@ -0,0 +1,13 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+# pre-load libraries
+library(flowCore)
+library(Rscattnlay)
+library(flowMie)
+library(wadeTools)
+library(fields)
+library(KernSmooth)
+
